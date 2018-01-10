be81af99fd88ac1d911a50e17142985056f9ccaf
Deepanshu Bagri <deepanshu.bagri98@gmail.com>
2018-01-11 03:17:43 +0530

    INITIAL

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ module.exports = {
     },
 
     mailing :{
-        from : "develop.bagri98@gmail.com",
-        password : "jaibhagwan"
+        from : "<from>",
+        password : "<password>"
     }
 }
